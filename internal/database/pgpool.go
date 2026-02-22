82a9a987e8049215da1344020a81062bbfee1293
Joe <gpliev777@gmail.com>
2026-02-22 10:11:32 +0300

    added pg pool set Up

---
+++ internal/database/pgpool.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPostgresPool(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
+	config, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse dsn: %w", err)
+	}
+
+	// Настройки пула
+	config.MaxConns = 25                      // Максимальное кол-во соединений
+	config.MinConns = 2                       // Минимальное кол-во соединений
+	config.MaxConnIdleTime = time.Minute * 10 // Время жизни простаивающего соединения
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping db: %w", err)
+	}
+
+	return pool, nil
+}
